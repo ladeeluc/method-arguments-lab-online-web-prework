a2e794ba4bf4db7068cdc22f1c7e01abc291c47b
Laura Hatch <laura.hatch.lh@gmail.com>
2019-07-10 23:39:55 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+ end
+ def introduction_with_language(element1,element2)
+   puts "Hi, my name is Dan and I am learning to program in Ember.js."
+ end
